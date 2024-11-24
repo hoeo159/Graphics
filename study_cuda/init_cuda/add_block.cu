f43aa4a323d4ce654139280572df043d4a5b9079
Birdman <hoeo159@gmail.com>
2024-11-24 18:50:51 +0900

    add common

--- study_cuda/init_cuda/add_block.cu
+++ study_cuda/init_cuda/add_block.cu
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <cuda_runtime.h>
+#include "../common/common.h"
 
 /*
     index = 2 * 256 + 3 (3번 block의 3번째 thread index)
@@ -22,36 +23,35 @@ int main(void)
 
     float *h_x = new float[N];
     float *d_x;
-    cudaMalloc(&d_x, N*sizeof(float));
-
     float *h_y = new float[N];
     float *d_y;
-    cudaMalloc(&d_y, N*sizeof(float));
+    
+    CUDA_CHECK(cudaMalloc(&d_x, N*sizeof(float)));
+    CUDA_CHECK(cudaMalloc(&d_y, N*sizeof(float)));
 
     for (int i = 0; i < N; i++) {
         h_x[i] = 1.0f;
         h_y[i] = 2.0f;
     }
 
-    cudaMemcpy(d_x, h_x, N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_y, h_y, N*sizeof(float), cudaMemcpyHostToDevice);
+    CUDA_CHECK(cudaMemcpy(d_x, h_x, N*sizeof(float), cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy(d_y, h_y, N*sizeof(float), cudaMemcpyHostToDevice));
 
     int blockSize = 256;
     int numBlocks = (N + blockSize - 1) / blockSize;
     add<<<numBlocks, blockSize>>>(N, d_x, d_y);
 
-    cudaDeviceSynchronize();
-    cudaMemcpy(h_y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+    CUDA_CHECK(cudaDeviceSynchronize());
+    CUDA_CHECK(cudaMemcpy(h_y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost));
 
     float maxError = 0.0f;
     for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(h_y[i]-3.0f));
+        maxError = fmax(maxError, fabs(h_y[i]-3.0f));
     std::cout << "Max error: " << maxError << std::endl;
 
     delete[] h_x;
     delete[] h_y;
-    cudaFree(d_x);
-    cudaFree(d_y);
-
+    CUDA_CHECK(cudaFree(d_x));
+    CUDA_CHECK(cudaFree(d_y));
     return 0;
 }
